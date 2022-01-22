44a284d90c160c1f1d28ab6985e38b5210ca5c15
Yassine Qoraiche <qoraicheofficiel@hotmail.com>
2022-01-22 16:48:04 +0100

    Update ProjectController.php

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -2,10 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\ProjectService;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
+    /**
+     * @var ProjectService
+     */
+    private $projectService;
+
+    /**
+     * ProjectController constructor.
+     * @param ProjectService $projectService
+     */
+    public function __construct(ProjectService $projectService)
+    {
+        $this->projectService = $projectService;
+    }
+
     public function __invoke()
     {
         // services
